6179bb7d06f16751cb377b235cb2c90e6935e8b0
samson ogbonna <samsonogbonna95@gmail.com>
2024-01-05 16:27:14 +0100

    final commit

--- base.js
+++ base.js
@@ -3,48 +3,53 @@ loadNews();
 // var url =
 //   "https://newsapi.org/v2/everything?q=tesla&apiKey=886b5f9aaf0643f1be5a9134a255e56e";
 
-document.getElementById("searchUsers").addEventListener("keyup", (e) => {
+document.getElementById("searchUsers").addEventListener("click", (e) => {
   var inputSearch = e.target.value;
   loadNews(inputSearch);
 });
 
 function loadNews(inputSearch = "") {
-  var users = [];
+  var newsVendor = [];
 
-  fetch("./news.json")
+  inputSearch = inputSearch ? inputSearch : "tesla";
+  let url = `https://newsapi.org/v2/everything?q=${inputSearch}&apiKey=886b5f9aaf0643f1be5a9134a255e56e`;
+
+  var fullPageString = document.getElementById("page_container");
+
+  fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log("users", data);
+      console.log("newsVendor", data);
 
       if (inputSearch) {
-        users = data.filter(function (item) {
-          return item.title.includes(inputSearch);
-        });
-        console.log(users);
+        newsVendor = data.articles;
+        fullPage.innerHTML = "";
+        console.log(newsVendor);
       } else {
-        users = data;
+        newsVendor = data.articles;
       }
 
-      var fullPageString = document.getElementById("page_container");
-      for (let i = 0; i < users.length; i++) {
-        var userElementString = `
+      for (let i = 0; i < newsVendor.length; i++) {
+        var newsElement = `
 
-        <figure>
-            <img src="images/team.jpg" alt="football team">
-            <a href="${users[i].url}">${users[i].title}</a>
-            <article>${users[i].description}</article>
-            <figcaption>${users[i].content}</figcaption>
-            <p>${users[i].publishedAt}</p>
+        <figure class="figure">
+            <img src="${
+              newsVendor[i].urlToImage ?? "images/team.jpg"
+            }" alt="football team">
+            <a href="${newsVendor[i].url}">${newsVendor[i].title}</a>
+            <article>${newsVendor[i].description}</article>
+            <figcaption>${newsVendor[i].content}</figcaption>
+            <p>${new Date(newsVendor[i].publishedAt).toDateString()}</p>
 
         </figure>
       `;
 
         const parser = new DOMParser();
-        var fullPageDiv = parser.parseFromString(userElementString, "text/html")
-          .body.firstChild;
-        fullPageString.appendChild(fullPageDiv);
+        var newsPageDiv = parser.parseFromString(newsElement, "text/html").body
+          .firstChild;
+        fullPage.append(newsPageDiv);
       }
     });
 }
